fcab5e9ae99747c08b6568c454d3789419291c0d
SureshKumar <sureshkumar5694@gmail.com>
2018-11-22 16:09:09 +0530

    Typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
 		if res.parsed_response['success'] == false
 			redirect_to login_failed_users_path and return
 		end	
-		redirect_to login_success	_users_path and return
+		redirect_to login_success_users_path
 	end
 
 	def login_success
